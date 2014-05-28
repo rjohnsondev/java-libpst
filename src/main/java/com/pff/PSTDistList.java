643a551c00f5d40bf87cd6a5ef14cb540be9ee8e
Richard Johnson <github@rjohnson.id.au>
2014-05-28 16:22:17 +0100

    Almost done with the distribution lists

--- src/main/java/com/pff/PSTDistList.java
+++ src/main/java/com/pff/PSTDistList.java
@@ -83,31 +83,28 @@ public class PSTDistList extends PSTMessage {
         public String addressType = "";
         public String emailAddress = "";
         int pos = 0;
+		public String toString() {
+			return String.format(
+				"Display Name: %s\n" +
+				"Address Type: %s\n" +
+				"Email Address: %s\n",
+				this.displayName,
+				this.addressType,
+				this.emailAddress);
+		}
     }
 
     private OneOffEntry parseOneOffEntry(byte[] data, int pos)
         throws IOException
     {
-        /*
-                   dd01 0f54 0200 0001 8064 0069 0073 0074  Ý..T.....d.i.s.t
-                   0020 006e 0061 006d 0065 0020 0032 0000  ...n.a.m.e...2..
-                   0053 004d 0054 0050 0000 0064 0069 0073  .S.M.T.P...d.i.s
-                   0074 0032 0040 0072 006a 006f 0068 006e  .t.2...r.j.o.h.n
-                   0073 006f 006e 002e 0069 0064 002e 0061  .s.o.n...i.d...a
-                   0075 0000 00 .u...
-                   */
         int version = (int)PSTObject.convertLittleEndianBytesToLong(data, pos, pos+2);
-        //System.out.println("Version: "+version);
         pos += 2;
 
         // http://msdn.microsoft.com/en-us/library/ee202811(v=exchg.80).aspx
         int additionalFlags = (int)PSTObject.convertLittleEndianBytesToLong(data, pos, pos+2);
-        //PSTObject.printFormattedNumber("Additional flags: ",additionalFlags);
         pos += 2;
 
-        //PSTObject.printFormattedNumber("flag: ",0x800);
         int pad = additionalFlags & 0x8000;
-        //PSTObject.printFormattedNumber("Pad: ",pad);
         int mae = additionalFlags & 0x0C00;
         int format = additionalFlags & 0x1E00;
         int m = additionalFlags & 0x0100;
@@ -121,21 +118,18 @@ public class PSTDistList extends PSTMessage {
         byte[] displayNameBytes = new byte[stringEnd - pos];
         System.arraycopy(data, pos, displayNameBytes, 0, displayNameBytes.length);
         String displayName = new String(displayNameBytes, "UTF-16LE");
-        //System.out.println("displayName: "+displayName);
         pos = stringEnd + 2;
 
         stringEnd = findNextNullChar(data, pos);
         byte[] addressTypeBytes = new byte[stringEnd - pos];
         System.arraycopy(data, pos, addressTypeBytes, 0, addressTypeBytes.length);
         String addressType = new String(addressTypeBytes, "UTF-16LE");
-        //System.out.println("addressType "+addressType);
         pos = stringEnd + 2;
 
         stringEnd = findNextNullChar(data, pos);
         byte[] emailAddressBytes = new byte[stringEnd - pos];
         System.arraycopy(data, pos, emailAddressBytes, 0, emailAddressBytes.length);
         String emailAddress = new String(emailAddressBytes, "UTF-16LE");
-        //System.out.println("emailAddress "+emailAddress);
         pos = stringEnd + 2;
 
         OneOffEntry out = new OneOffEntry();
@@ -146,30 +140,19 @@ public class PSTDistList extends PSTMessage {
         return out;
     }
 
-	public String[] getDistributionListMembers()
+	public Object[] getDistributionListMembers()
 		throws PSTException, IOException
 	{
-		//PidLidDistributionListMembers
-		// this looks like a Recipeint One-Off EntryID Structure
 		PSTTableBCItem item = this.items.get(pstFile.getNameToIdMapItem(0x8055, PSTFile.PSETID_Address));
-		//item = this.items.get(0x8047);
-		String[] out = {};
+		Object[] out = {};
         if (item != null) {
-            PSTObject.printHexFormatted(item.data, true);
-
             int pos = 0;
-
             int count = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+4);
-            //System.out.println("Count: "+count);
+			out = new Object[count];
             pos += 4;
             pos = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+4);
-            //System.out.println("pos: "+pos);
 
-            while (pos < item.data.length) {
-                /*
-                   00 0000 00
-                   81 2b1f a4be a310 199d 6e00 dd01 0f54
-                   */
+			for (int x = 0; x < count; x++) {
                 // http://msdn.microsoft.com/en-us/library/ee218661(v=exchg.80).aspx
                 // http://msdn.microsoft.com/en-us/library/ee200559(v=exchg.80).aspx
                 int flags = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+4);
@@ -177,51 +160,36 @@ public class PSTDistList extends PSTMessage {
 
                 byte[] guid = new byte[16];
                 System.arraycopy(item.data, pos, guid, 0, guid.length);
-                //PSTObject.printHexFormatted(guid, true);
-                //System.out.println(Arrays.equals(guid, wrappedEntryIdUid));
                 pos += 16;
 
                 if (Arrays.equals(guid, wrappedEntryIdUid)) {
-                    //System.out.println("Wrapped entry");
-
                     /* c3 */
                     int entryType = item.data[pos] & 0x0F;
                     int entryAddressType = item.data[pos] & 0x70 >> 4;
                     boolean isOneOffEntryId = (item.data[pos] & 0x80) > 0;
                     pos++;
-                    if (entryType == 3) {
-                        /*
-                           00 0000 00
-                           a4 1d63 dbc5 3b8e 4ab8 071e <- some guid from some where.
-                           15e5 5750 ce
-                           64 00 20 00
-                           */
-                        int wrappedflags = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+4);
-                        pos += 4;
+					int wrappedflags = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+4);
+					pos += 4;
 
-                        byte[] guid2 = new byte[16];
-                        System.arraycopy(item.data, pos, guid, 0, guid.length);
-                        //PSTObject.printHexFormatted(guid, true);
-                        pos += 16;
+					byte[] guid2 = new byte[16];
+					System.arraycopy(item.data, pos, guid, 0, guid.length);
+					pos += 16;
 
-                        int descriptorId = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+3);
-                        pos += 3;
+					int descriptorIndex = (int)PSTObject.convertLittleEndianBytesToLong(item.data, pos, pos+3);
+					pos += 3;
 
-                        byte empty = item.data[pos];
-                        pos++;
+					byte empty = item.data[pos];
+					pos++;
 
-                    }
+					out[x] = PSTObject.detectAndLoadPSTObject(this.pstFile, descriptorIndex);
 
                 } else if (Arrays.equals(guid, oneOffEntryIdUid)) {
                     OneOffEntry entry = parseOneOffEntry(item.data, pos);
-                    System.out.println(entry.emailAddress);
                     pos = entry.pos;
+					out[x] = entry;
                 }
-
             }
 		}
 		return out;
 	}
-
-
 }
--- src/test/java/com/pff/DistListTest.java
+++ src/test/java/com/pff/DistListTest.java
@@ -22,61 +22,66 @@ public class DistListTest {
      */
     @Test
     public final void testGetDistList() {
-		try {
-			URL dir_url = ClassLoader.getSystemResource("dist-list.pst");
-			PSTFile pstFile = new PSTFile(new File(dir_url.toURI()));
-			PSTDistList obj = (PSTDistList)PSTObject.detectAndLoadPSTObject(pstFile, 2097188);
-			System.out.println(obj);
-			String[] members =obj.getDistributionListMembers();
-			for (String member : members) {
-				System.out.println(member);
-			}
+        try {
+            URL dir_url = ClassLoader.getSystemResource("dist-list.pst");
+            PSTFile pstFile = new PSTFile(new File(dir_url.toURI()));
+            PSTDistList obj = (PSTDistList)PSTObject.detectAndLoadPSTObject(pstFile, 2097188);
+            System.out.println(obj);
+            Object[] members = obj.getDistributionListMembers();
+            for (Object member : members) {
+                if (member instanceof PSTContact) {
+                    PSTContact contact = (PSTContact)member;
+                    System.out.println(contact);
+                } else {
+                    System.out.println(member);
+                }
+            }
 
-			//System.out.println(pstFile.getMessageStore().getDisplayName());
-			//processFolder(pstFile.getRootFolder());
-		} catch (Exception err) {
-			err.printStackTrace();
-		}
+            //System.out.println(pstFile.getMessageStore().getDisplayName());
+            //processFolder(pstFile.getRootFolder());
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
         //org.junit.Assert.assertTrue(false);
     }
 
-	int depth = -1;
-	public void processFolder(PSTFolder folder)
-			throws PSTException, java.io.IOException
-	{
-		depth++;
-		// the root folder doesn't have a display name
-		if (depth > 0) {
-			printDepth();
-			System.out.println(folder.getDisplayName());
-		}
+    int depth = -1;
+    public void processFolder(PSTFolder folder)
+            throws PSTException, java.io.IOException
+    {
+        depth++;
+        // the root folder doesn't have a display name
+        if (depth > 0) {
+            printDepth();
+            System.out.println(folder.getDisplayName());
+        }
 
-		// go through the folders...
-		if (folder.hasSubfolders()) {
-			Vector<PSTFolder> childFolders = folder.getSubFolders();
-			for (PSTFolder childFolder : childFolders) {
-				processFolder(childFolder);
-			}
-		}
+        // go through the folders...
+        if (folder.hasSubfolders()) {
+            Vector<PSTFolder> childFolders = folder.getSubFolders();
+            for (PSTFolder childFolder : childFolders) {
+                processFolder(childFolder);
+            }
+        }
 
-		// and now the emails for this folder
-		if (folder.getContentCount() > 0) {
-			depth++;
-			PSTMessage email = (PSTMessage)folder.getNextChild();
-			while (email != null) {
-				printDepth();
-				System.out.println("Email: "+email.getDescriptorNodeId()+" "+email.getSubject());
-				email = (PSTMessage)folder.getNextChild();
-			}
-			depth--;
-		}
-		depth--;
-	}
+        // and now the emails for this folder
+        if (folder.getContentCount() > 0) {
+            depth++;
+            PSTMessage email = (PSTMessage)folder.getNextChild();
+            while (email != null) {
+                printDepth();
+                System.out.println("Email: "+email.getDescriptorNodeId()+" "+email.getSubject());
+                email = (PSTMessage)folder.getNextChild();
+            }
+            depth--;
+        }
+        depth--;
+    }
 
-	public void printDepth() {
-		for (int x = 0; x < depth-1; x++) {
-			System.out.print(" | ");
-		}
-		System.out.print(" |- ");
-	}
+    public void printDepth() {
+        for (int x = 0; x < depth-1; x++) {
+            System.out.print(" | ");
+        }
+        System.out.print(" |- ");
+    }
 }

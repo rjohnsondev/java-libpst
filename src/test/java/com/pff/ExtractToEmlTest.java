46822a52210669225e1838016cc8ee7bc72a8c31
inter6 <inter6@naver.com>
2014-12-10 13:05:11 +0900

    Modify ExtractToEmlTest - Improve EML file name - Add MIME headers to EML - Safely EML path

--- src/test/java/com/pff/ExtractToEmlTest.java
+++ src/test/java/com/pff/ExtractToEmlTest.java
@@ -8,11 +8,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.Stack;
 
+import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
@@ -35,7 +39,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ExtractToEmlTest {
 
-	private final static String INPUT_PST_FILE = "E:/eml/pst/inter6@naver.com.pst";
+	private final static String INPUT_PST_FILE = "E:/eml/pst/inter6@daou.co.kr.pst";
 	private final static String OUTPUT_EML_DIR = "E:/output";
 
 	@Test
@@ -75,6 +79,7 @@ public class ExtractToEmlTest {
 			return;
 		}
 
+		File outputDir = generateOutputDir(currentFolder, parentFolderStack);
 		for (int idx = 0; idx < size; idx++) {
 			try {
 				PSTObject object = currentFolder.getNextChild();
@@ -85,13 +90,11 @@ public class ExtractToEmlTest {
 					throw new IllegalArgumentException("is not PSTMessage ! - IDX:" + idx + " JAVA_CLASS:" + object.getClass().getName());
 				}
 
-				File outputDir = generateOutputDir(currentFolder, parentFolderStack);
 				PSTMessage msg = (PSTMessage) object;
 				MimeMessage mime = createMime(msg);
 				OutputStream os = null;
 				try {
-					// TODO improve filename
-					os = new FileOutputStream(new File(outputDir, idx + ".eml"));
+					os = new FileOutputStream(new File(outputDir, generateEmlFilename(msg, idx)));
 					mime.writeTo(os);
 				} finally {
 					closeQuitely(os);
@@ -103,10 +106,60 @@ public class ExtractToEmlTest {
 		}
 	}
 
-	private MimeMessage createMime(PSTMessage msg) throws MessagingException, PSTException, IOException {
-		MimeMessage mime = new MimeMessage(Session.getDefaultInstance(new Properties()));
-		// TODO build header
+	private String generateEmlFilename(PSTMessage msg, int idx) {
+		Date date = msg.getMessageDeliveryTime();
+		if (date == null) {
+			date = new Date();
+		}
+		String subject = msg.getSubject();
+		if (isBlank(subject)) {
+			subject = "no_subject";
+		}
+		subject = makeValidFilename(subject, "_");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd_HHmmss");
+		return idx + "-" + sdf.format(date) + "-" + subject + ".eml";
+	}
 
+	private MimeMessage createMime(PSTMessage msg) throws MessagingException, PSTException, IOException {
+		MimeMessage mime = new MyMimeMessage(Session.getDefaultInstance(new Properties()));
+		buildMimeHeader(msg, mime);
+		buildMimePart(msg, mime);
+		mime.saveChanges();
+		return mime;
+	}
+
+	private void buildMimeHeader(PSTMessage msg, MimeMessage mime) throws MessagingException, UnsupportedEncodingException, PSTException, IOException {
+		// From
+		mime.setFrom(new InternetAddress(msg.getSenderEmailAddress(), msg.getSenderName(), "utf-8"));
+		// To, Cc, Bcc
+		for (int recvIdx = 0; recvIdx < msg.getNumberOfRecipients(); recvIdx++) {
+			PSTRecipient recipient = msg.getRecipient(recvIdx);
+			RecipientType type = null;
+			switch (recipient.getRecipientType()) {
+				case PSTRecipient.MAPI_TO:
+					type = RecipientType.TO;
+					break;
+				case PSTRecipient.MAPI_CC:
+					type = RecipientType.CC;
+					break;
+				case PSTRecipient.MAPI_BCC:
+					type = RecipientType.BCC;
+					break;
+				default:
+					type = RecipientType.TO; // force TO
+					break;
+			}
+			mime.addRecipient(type, new InternetAddress(recipient.getEmailAddress(), recipient.getDisplayName(), "utf-8"));
+		}
+		// Date
+		mime.setSentDate(msg.getMessageDeliveryTime());
+		// Subject
+		mime.setSubject(msg.getSubject(), "utf-8");
+		// Message-ID
+		mime.setHeader("Message-ID", msg.getInternetMessageId());
+	}
+
+	private void buildMimePart(PSTMessage msg, MimeMessage mime) throws MessagingException, PSTException, IOException, UnsupportedEncodingException {
 		MimeMultipart rootMp = new MimeMultipart("mixed");
 		{
 			// build message part
@@ -136,20 +189,20 @@ public class ExtractToEmlTest {
 			}
 		}
 		mime.setContent(rootMp);
-		mime.saveChanges();
-		return mime;
 	}
 
 	private String encodedFilename(String filename) throws UnsupportedEncodingException {
-		String plain = filename;
-		if (plain == null) {
-			plain = "unknown";
+		if (isBlank(filename)) {
+			return "unknown";
 		}
-		plain = plain.trim();
-		if ("".equals(plain)) {
-			plain = "unknown";
+		return MimeUtility.encodeWord(filename, "utf-8", "B");
+	}
+
+	private boolean isBlank(String str) {
+		if (str == null) {
+			return true;
 		}
-		return MimeUtility.encodeWord(plain, "utf-8", "B");
+		return "".equals(str.trim());
 	}
 
 	private byte[] getAttachBytes(PSTAttachment attach) throws IOException, PSTException {
@@ -172,14 +225,20 @@ public class ExtractToEmlTest {
 		StringBuilder sb = new StringBuilder();
 		sb.append(OUTPUT_EML_DIR);
 		for (PSTFolder parentFolder : parentFolderStack) {
-			sb.append("/" + parentFolder.getDisplayName());
+			sb.append("/" + makeValidFilename(parentFolder.getDisplayName(), "_"));
 		}
-		sb.append("/" + currentFolder.getDisplayName());
+		sb.append("/" + makeValidFilename(currentFolder.getDisplayName(), "_"));
 
-		// TODO safely path name
 		File outputDir = new File(sb.toString());
 		outputDir.mkdirs();
 		return outputDir;
+	}
+
+	private String makeValidFilename(String fileName, String replaceStr) {
+		if (isBlank(fileName) || replaceStr == null) {
+			return "";
+		}
+		return fileName.replaceAll("[:\\\\/%*?:|\"<>]", replaceStr);
 	}
 
 	private void closeQuitely(Closeable closeable) {
@@ -203,5 +262,26 @@ public class ExtractToEmlTest {
 
 	private String getFolderInfo(PSTFolder folder) {
 		return "[" + folder.getDisplayName() + "(" + folder.getContentCount() + ")]";
+	}
+
+	/**
+	 * Prevent update Message-ID
+	 * 
+	 * @author inter6
+	 *
+	 */
+	private class MyMimeMessage extends MimeMessage {
+
+		public MyMimeMessage(Session session) {
+			super(session);
+		}
+
+		@Override
+		protected void updateMessageID() throws MessagingException {
+			String[] ids = getHeader("Message-ID");
+			if (ids == null || ids.length == 0 || isBlank(ids[0])) {
+				super.updateMessageID();
+			}
+		};
 	}
 }
